57b4301703be94986dd6e8faf961e2940a4f2c05
vcp3k <81894130+vcp3k@users.noreply.github.com>
2021-04-04 00:07:33 -0500

    Add files via upload

---
+++ app/app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Em on 3/04/21.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
